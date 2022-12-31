a0c18ece33095f73c4db4a1abebd3f52edbaeb54
Aroskar17 <93914017+Aroskar17@users.noreply.github.com>
2022-12-31 19:58:03 +0530

    Update DBMSProject.java

--- DBMSProject.java
+++ DBMSProject.java
@@ -1,23 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package dbmsproject;
 
 import dbmsproject.HomeFrame;
 import dbmsproject.Connector;
 import java.awt.*;
 
-/**
- *
- * @author ADNAN
- */
+
 public class DBMSProject {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     Boolean user_login = false;
 
     public static void main(String[] args) {
